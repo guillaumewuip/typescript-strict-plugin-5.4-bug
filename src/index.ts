d3751cb0fde410fadc72186d9d209a0569baddc8
Guillaume Clochard <guillaume.clochard@blablacar.com>
2024-03-17 14:15:17 +0100

    fixup! init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-
-
 function test(params: { toto: string, tata: number }) {
   console.log(params);
 }
 
+// We expect TS error here:
+//   Object literal may only specify known properties,
+//   and 'titi' does not exist in type '{ toto: string; tata: number; }'.
 test({ titi: 'yoooo' })
